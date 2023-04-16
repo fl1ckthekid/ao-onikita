1c3b6ea0105ca794afe4c03f62362ead6778b847
nolife99 <chendylan32@gmail.com>
2023-04-16 19:39:58 -0400

    fix a script copypaste

--- RGSS3/Window_Help.rb
+++ RGSS3/Window_Help.rb
@@ -1,20 +1,21 @@
-class Window_Target < Window_Selectable
+class Window_Help < Window_Base
   def initialize
-    super(0, 0, 336, 480)
+    super(0, 0, 640, 64)
     self.contents = Bitmap.new(width - 32, height - 32)
-    self.z += 10
-    @item_max = $game_party.actors.size
-    refresh
   end
-  def refresh
-  end
-  def update_cursor_rect
-    if @index <= -2
-      self.cursor_rect.set(0, (@index + 10) * 116, self.width - 32, 96)
-    elsif @index == -1
-      self.cursor_rect.set(0, 0, self.width - 32, @item_max * 116 - 20)
-    else
-      self.cursor_rect.set(0, @index * 116, self.width - 32, 96)
+  def set_text(text, align = 0)
+    if text != @text or align != @align
+      self.contents.clear
+      self.contents.font.color = normal_color
+      self.contents.draw_text(4, 0, self.width - 40, 32, text, align)
+      @text = text
+      @align = align
+      @actor = nil
     end
+    self.visible = true
+  end
+  def set_actor(actor)
+  end
+  def set_enemy(enemy)
   end
 end
--- RGSS3/Window_Target.rb
+++ RGSS3/Window_Target.rb
@@ -1,8 +1,20 @@
 class Window_Target < Window_Selectable
   def initialize
+    super(0, 0, 336, 480)
+    self.contents = Bitmap.new(width - 32, height - 32)
+    self.z += 10
+    @item_max = $game_party.actors.size
+    refresh
   end
   def refresh
   end
   def update_cursor_rect
+    if @index <= -2
+      self.cursor_rect.set(0, (@index + 10) * 116, self.width - 32, 96)
+    elsif @index == -1
+      self.cursor_rect.set(0, 0, self.width - 32, @item_max * 116 - 20)
+    else
+      self.cursor_rect.set(0, @index * 116, self.width - 32, 96)
+    end
   end
 end
